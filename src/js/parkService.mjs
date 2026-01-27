c58368ce9317334b5777374bd6de59ea7a2eb622
mal-prog <poteramalcolm@gmail.com>
2026-01-27 15:23:02 -0700

    fini

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -158,23 +158,6 @@ const park = {
   designation: "National Park"
 };
 
-export async function getParkData() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-Api-Key": apiKey
-    }
-  };
-  let data = {};
-  const response = await fetch(baseUrl + "parks" + "?parkCode=yell", options);
-  // check to make sure the reponse was ok.
-  if (response.ok) {
-    // convert to JSON
-    data = await response.json();
-  } else throw new Error("response not ok");
-  // return just the first row of the data object
-  return data.data[0];
-}
 
 export async function getVisitorCenterData() {
   const options = {
